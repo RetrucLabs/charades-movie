ea612ac00cfd89501bc665777356f5191c1cd511
José Luis Sánchez <joseluis.sanchezb@gmail.com>
2016-01-08 22:20:24 +0100

    Added list of films method

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,23 @@
 
 const logger        = require('../lib/logger');
+const _             = require('underscore');
+const validator     = require('validator');
 
-module.exports = function(express) {
+module.exports = (express) => {
   express.get('/', getRoot);
+  express.get('/films', getFilmsList);
 };
 
 function getRoot(req, res) {
   logger.info('Url requested: /', req.baseUrl);
   res.status(200).send('Hello! I\'m the movie charades microservice');
+}
+
+function getFilmsList(req, res) {
+  var outputLength = req.query.ol;
+  if(outputLength && !validator.isNumeric(req.query.ol)) return res.status(400).send('Invalid param');
+  if(!outputLength) outputLength = 5;
+  var films = _.shuffle(require('../lib/films.json'));
+  if(films.length > outputLength) films = films.splice(0, outputLength);
+  res.status(200).json(films);
 }
